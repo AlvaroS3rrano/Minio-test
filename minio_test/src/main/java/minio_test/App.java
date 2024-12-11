bedb9b4de1e79b8df4df308c61e3b8f2945a485d
Álvaro Serrano <117301012+AlvaroS3rrano@users.noreply.github.com>
2024-12-11 09:15:37 +0100

    creating a bucket

--- minio_test/src/main/java/minio_test/App.java
+++ minio_test/src/main/java/minio_test/App.java
@@ -1,17 +1,28 @@
 package minio_test;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.ObjectWriteResponse;
 import io.minio.UploadObjectArgs;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
 import io.minio.errors.MinioException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
 import io.minio.messages.Bucket;
 
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		MinioClient minioClient = demo();
+		MinioClient minioClient = demo(); // Conectamos con minio 
 		try {
 			List<Bucket> bList = minioClient.listBuckets();
 			System.out.println("Connection success, total buckets: "+bList.size());
@@ -20,11 +31,16 @@ public class App {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		try {
+			createBucket(minioClient);
+		} catch (MinioException e) {
+			System.out.println("The bucket already exists");
+		}
 		upload(minioClient);
 
 	}
 	
+	// to connect with minio
 	private static MinioClient demo() {
 		MinioClient minioClient = MinioClient.builder()
 				.endpoint("http://127.0.0.1:9000")
@@ -33,6 +49,7 @@ public class App {
 		return minioClient;
 	}
 	
+	// to upload files
 	private static void upload(MinioClient minioClient) throws Exception {
 		String bucketName = "hola";
 		String objectName = "trial.txt";
@@ -45,6 +62,25 @@ public class App {
 		ObjectWriteResponse resp = minioClient.uploadObject(uArgs);
 		
 		System.out.println(resp.object() + ": "+ resp.etag()+": "+resp.versionId());
+	}
+	
+	// to create a bucket
+	private static void createBucket(MinioClient minioClient) throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
+		String bucketName = "hola";
+		MakeBucketArgs mbArgs = MakeBucketArgs.builder() // otras opciones podrian ser region 
+				.bucket(bucketName)						 // objectLock ...
+				.build();
+		minioClient.makeBucket(mbArgs); // no devuelve nada
+		
+		BucketExistsArgs beArgs = BucketExistsArgs.builder()
+				.bucket(bucketName)
+				.build();
+		
+		if (minioClient.bucketExists(beArgs)) {
+			System.out.println("Bucket " + bucketName + " exists");
+		} else {
+			System.out.println("Bucket " + bucketName + " does not exists");
+		}
 	}
 
 }
